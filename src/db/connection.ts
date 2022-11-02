bd2411174c2de5ffca53f3988718a4df18e1d455
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2022-11-02 13:14:16 +0530

    eslint fix

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,13 +2,13 @@ import { Sequelize } from "sequelize-typescript";
 import { User } from "../models/User";
 
 const connection = new Sequelize({
-	dialect: "postgres",
-	host: process.env.SEQUELIZE_DB_HOST,
-	username: process.env.SEQUELIZE_DB_USERNAME,
-	password: process.env.SEQUELIZE_DB_PASSWORD,
-	database: process.env.SEQUELIZE_DB_DATABASE,
-	logging: false,
-	models: [User],
+  dialect: "postgres",
+  host: process.env.SEQUELIZE_DB_HOST,
+  username: process.env.SEQUELIZE_DB_USERNAME,
+  password: process.env.SEQUELIZE_DB_PASSWORD,
+  database: process.env.SEQUELIZE_DB_DATABASE,
+  logging: false,
+  models: [User],
 });
 
 export default connection;
